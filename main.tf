0001de46a49129a851474d3251db5b833e4ff522
Aliu Raimi Olatunde <raimialiu428@gmail.com>
2021-12-15 09:15:22 +0100

    terraform modules

---
+++ main.tf
@@ -0,0 +1,26 @@
+variable "filedirecatory" {
+  description = "location of the files to be archived"
+}
+
+
+variable "archiveformat" {
+  description = "format of archive"
+}
+variable "outputpath" {
+  description = "path the archive will be stored"
+}
+
+data "archive_file" "archivefile" {
+    type = var.archiveformat
+    output_path = var.outputpath
+    source_file = var.filedirecatory
+  
+}
+
+module "archivefile" {
+  
+}
+
+output "result" {
+  value = data.archive_file.archivefile
+}
